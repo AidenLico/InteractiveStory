b246ac6a69c128e219ea9656d893327989503d4b
LiamTrodden1 <liamtrodden28@gmail.com>
2022-09-28 17:28:17 +0100

    Complete

---
+++ mains.py
@@ -0,0 +1,70 @@
+### I'll do my parts of the story and you do your's
+#####################################################
+
+#title
+print("="*11)
+print("The Iron Fist")
+print("="*11)
+
+#intro
+print("Will was an honest working programmer who was well respected in his community for his life changing",
+"evolution in technology, particularly in the medical sector with highly specialized medic bots capable of",
+"the most intricate surgery with close to 0 unsuccessful surgeries.",
+"For this reason, the iron fist, an almost unstoppable autonomous army, which aims to create the",
+"“perfect” civilization, desired his skills. Will could resist this knowing his skills would be used to destroy",
+"the fleeting towns and villages that resisted but this may come at the cost of his own life.")
+
+# choice to rebel or join (choice1)
+choice1 = input("(Go to 1a to resist or 1b to confide with the iron fist)")
+if choice1 == "1a":
+    print("")
+    print("1a) After the iron fist concluded Will would not cooperate, he was detained and placed in the iron gate",
+    "prison. The iron gate prison was a highly secure prison with the sole purpose of removing those who",
+    "rebelled against the iron fist.")
+    print("Will was faced with 2 options the ultimatum was, face death with a lethal dosage poison or attempt a",
+    "trial randomly decided by the prison computer system which broadcasts to the public the horrors they",
+    "will face if they disobey the iron fist. Will chose the second option as this would give him a small chance",
+    "of survival despite it being very slim, he deemed it to be the more rational option.")
+if choice1 == "1b":
+    print("1b) Will cowardly chose to join the iron fist. Within a few swift weeks the first batch of iron fist medic",
+    "bots were created. Will knew the medic bots were faulty, yet the iron fist didn’t seem to care. He became",
+    "less and less essential and more and more outspoken. In a failed attempt from a resistance, Will, who",
+    "was housed near the outer perimeter of the fort, would be critically injured. At which point a medic bot",
+    "began live surgery. However, due to the unfinished design, the bot lacked the precision to stitch Will's",
+    "wounds, and soon Will was dead.")
+
+# computer decides trial
+print("In a robotic voice the computer exclaimed select door 1, 2 or 3. A loud one toned buzz deafened Will as a",
+"large screen that spanned the entire rusted wall booted up revealing the message of the trial Will would",
+"face. The message read prisoner will be sentenced to --------- the --------- whilst --------. Like a slot",
+"machine, words rolled past the empty spaces ready to fill in the blanks. As the words slowed, he could",
+"make out a few of them like race, escape and outrun. Finally, the full sentence was locked in… survive",
+"the arena whilst unequipped",
+"A large iron gate opened to a dimly lit metal corridor in which Will entered. The air froze as Will saw his",
+"first obstacle (Continue reading to proceed with door 1 or skip to door 2 or door 3).")
+
+###########################################
+# Door 1 ( your part )
+choice2 = input("(Go to door1 door2 or door3)")
+if choice2 == "door1":
+    print("")
+###########################################    
+if choice2 == "door2":
+    print("")
+    print("Door 2) Will began to sweat as he approached the void like room with an almost endless vertical drop. At",
+    "the bottom of the pit was a vast pool of molten iron which dimly illuminated the room.",
+    "Across the center was one singular path Will would have to cross to exit the room.",
+    "Without taking a breath Will wobbled over the rope worried he would fall. He reached a larger platform",
+    "for and for a brief moment and re-collected his thoughts. The next obstacle was a beam elevated above",
+    "with handles to climb the gap with great effort Will had traversed across to the next obstacle, a large",
+    "sheet metal wall with small gaps to fit fingers and feet in order to climb.",
+    "Will could see scratches and indents in the metal which looked to be caused by a type of climbing tool. In",
+    "his brief rest to evaluate his situation, a spinning red light activated as well as a metal piston that began",
+    "to slowly slide the wall behind him forwards. Now under the time constraint of the wall Will began to",
+    "panic. Will decides too...")
+##############################################
+# door 3 ( your part )
+if choice2 == "door3":
+    print("")
+
+

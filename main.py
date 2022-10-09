bcba2b9a02a3a52da9ab2b5589eb21c9644e3fab
AidenLico <aiden.lico@gmail.com>
2022-10-09 23:15:54 +0100

    aaaaaa

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 ### Main file
 ### I'll do my parts of the story and you do your's
 #####################################################
+#imports
+import time
+### please comment any time.sleep commands for testing as they are only for
+###user convenience, slows down testing!!!
+
+#function for choices
+def choiceFunc(choice):
+    listOfChoices = ["1a) After the iron fist concluded Will would not cooperate, he was detained and placed in the iron gate prison. The iron gate prison was a highly secure prison with the sole purpose of removing those who rebelled against the iron fist. Will was faced with 2 options the ultimatum was, face death with a lethal dosage poison or attempt a trial randomly decided by the prison computer system which broadcasts to the public the horrors they will face if they disobey the iron fist. Will chose the second option as this would give him a small chance of survival despite it being very slim, he deemed it to be the more rational option.",
+                     "1b) Will cowardly chose to join the iron fist. Within a few swift weeks the first batch of iron fist medic bots were created. Will knew the medic bots were faulty, yet the iron fist didn’t seem to care. He became less and less essential and more and more outspoken. In a failed attempt from a resistance, Will, who was housed near the outer perimeter of the fort, would be critically injured. At which point a medic bot began live surgery. However, due to the unfinished design, the bot lacked the precision to stitch Will's wounds, and soon Will was dead.",
+                     "Door 1) At the end of the corridor was an opening in which there appeared to be some sort of metal behemoth, in which he could just about make out in the dimly lit entrance. As he entered the arena, a cylindrical metal chamber, the exit he had just left closed behind him. He was sealed in with this iron beast. Although inactive it was still a terrifying sight to see such a thing. In a flash, lights turned on illuminating the arena in a yellow glow, and maybe 200m away was another contender on either side of him.\n“SURVIVE FOR 60 SECONDS IN ORDER TO PASS… 10, 9, 8, 7, 6…”\nA metal whirring began as neon bulbs activated all around the giant. Its hinges creaked as it began to stand, dust and debris falling off its body.\n“5, 4…”\nIts eyes shone a bright red as its head swiveled around glaring at every poor soul trapped with it.\n“3, 2, 1..”\nIt charges towards its first target, fortunately giving Will time to come to your senses and dart for the opposite end.The candidate it had gone for was not so fortunate, frozen still as the beast hurls its fist - crashing down upon where they once stood.\n“45 seconds”\nAs if it was some instinctive animal, it next charges at Will, although he would not succumb to fear and darts as fast as he can. The wind howls as its fist tries to end him, barely avoiding Will as he sprints away. Clearly distressed by its failure it lets out a screech.\n“30 seconds”\nIt seems to lose interest in Will momentarily, as the other candidate was banging on the metal door in which he had come from. Not a moment later, it brings its massive foot the size of a car crashing down upon him. You were the last one standing…\n",
+                     "Door 2) Will began to sweat as he approached the void like room with an almost endless vertical drop. At the bottom of the pit was a vast pool of molten iron which dimly illuminated the room. Across the center was one singular path Will would have to cross to exit the room. Without taking a breath Will wobbled over the rope worried he would fall. He reached a larger platform and for a brief moment and re-collected his thoughts. The next obstacle was a beam elevated above with handles to climb the gap with great effort Will had traversed across to the next obstacle, a large sheet metal wall with small gaps to fit fingers and feet in order to climb. Will could see scratches and indents in the metal which looked to be caused by a type of climbing tool. In his brief rest to evaluate his situation, a spinning red light activated as well as a metal piston that began to slowly slide the wall behind him forwards. Now under the time constraint of the wall Will began to panic. Will decides too...",
+                     "Will entered and was immediately punctured by a dart firing directly into his back, and he immediately felt a great burning pain as though his skin was on fire. In front, an extremely deep pool was made apparent to him, the bottom appearing so far away he could barely make out that there was a metal box at the bottom of the pool..\n“FOR THIS TASK YOU MUST SWIM TO THE BOTTOM, AND UNLOCK THE SAFE, THE SAFE IS LOCKED A SET OF LOGIC GATES IN WHICH MUST BE ALIGNED CORRECTLY TO UNLOCK THE ANTIDOTE TO THE DART YOU WERE SHOT WITH.”\nHe looks down into the water barely lit up with equally spaced bulbs lining the side of the pool. Without hesitation, he dives right into the pool and swims as fast as he physically could downwards. The pressure became greater and greater and Will found it more and more of a struggle until finally he reached the bottom. He fought to remain at the bottom and looked at the logic gate on the side of the metal box. It had 2 inputs (A and B) which could be set to 0 or 1, A was followed by a NOT gate and then both A and B met at an AND gate. Will started to feel his chest tighten as he fought to hold his breath. Will fumbles and quickly begins setting the inputs."]
+
+    return listOfChoices[choice]
+                     
+                    
+                     
 
 #title
 print("="*11)
 print("The Iron Fist")
 print("="*11)
+
+time.sleep(1)
 
 #intro
 print("Will was an honest working programmer who was well respected in his community for his life changing",
@@ -15,24 +34,28 @@ print("Will was an honest working programmer who was well respected in his commu
 "“perfect” civilization, desired his skills. Will could resist this knowing his skills would be used to destroy",
 "the fleeting towns and villages that resisted but this may come at the cost of his own life.")
 
+time.sleep(5)
+
 # choice to rebel or join (choice1)
-choice1 = input("(Go to 1a to resist or 1b to confide with the iron fist)")
-if choice1 == "1a":
-    print("")
-    print("1a) After the iron fist concluded Will would not cooperate, he was detained and placed in the iron gate",
-    "prison. The iron gate prison was a highly secure prison with the sole purpose of removing those who",
-    "rebelled against the iron fist.")
-    print("Will was faced with 2 options the ultimatum was, face death with a lethal dosage poison or attempt a",
-    "trial randomly decided by the prison computer system which broadcasts to the public the horrors they",
-    "will face if they disobey the iron fist. Will chose the second option as this would give him a small chance",
-    "of survival despite it being very slim, he deemed it to be the more rational option.")
-if choice1 == "1b":
-    print("1b) Will cowardly chose to join the iron fist. Within a few swift weeks the first batch of iron fist medic",
-    "bots were created. Will knew the medic bots were faulty, yet the iron fist didn’t seem to care. He became",
-    "less and less essential and more and more outspoken. In a failed attempt from a resistance, Will, who",
-    "was housed near the outer perimeter of the fort, would be critically injured. At which point a medic bot",
-    "began live surgery. However, due to the unfinished design, the bot lacked the precision to stitch Will's",
-    "wounds, and soon Will was dead.")
+check = False
+                     
+while check == False:
+    choice1 = input("(Go to 1a to resist or 1b to confide with the iron fist)")
+    
+    if choice1 == "1a":
+        output = choiceFunc(0)
+        print(output)
+        check = True
+                     
+    elif choice1 == "1b":
+        output = choiceFunc(1)
+        print(output)
+        exit()
+
+    else:
+        print("Invalid option")
+                     
+time.sleep(5)
 
 # computer decides trial
 print("In a robotic voice the computer exclaimed select door 1, 2 or 3. A loud one toned buzz deafened Will as a",
@@ -44,63 +67,30 @@ print("In a robotic voice the computer exclaimed select door 1, 2 or 3. A loud o
 "A large iron gate opened to a dimly lit metal corridor in which Will entered. The air froze as Will saw his",
 "first obstacle (Continue reading to proceed with door 1 or skip to door 2 or door 3).")
 
-###########################################
-# Door 1 ( your part )
-choice2 = input("(Go to door1 door2 or door3)")
-if choice2 == "door1":
-    print("")
-    print("Door1) At the end of the corridor was an opening in which there appeared to be some sort of metal behemoth,",
-          "in which he could just about make out in the dimly lit entrance. As he entered the arena, a cylindrical metal ",
-          "chamber, the exit he had just left closed behind him. He was sealed in with this iron beast. Although inactive ",
-          "it was still a terrifying sight to see such a thing. In a flash, lights turned on illuminating the arena in a ",
-          "yellow glow, and maybe 200m away was another contender on either side of him.")
-    print("")
-    print("“SURVIVE FOR 60 SECONDS IN ORDER TO PASS… 10, 9, 8, 7, 6…”")
-    print("")
-    print("A metal whirring began as neon bulbs activated all around the giant. Its hinges creaked as it began to stand, ",
-          "dust and debris falling off its body.")
-    print("“5, 4…”")
-    print("Its eyes shone a bright red as its head swiveled around glaring at every poor soul trapped with it.")
-    print("“3, 2, 1..”")
-    print("It charges towards its first target, fortunately giving Will time to come to your senses and dart for the opposite end.",
-          "The candidate it had gone for was not so fortunate, frozen still as the beast hurls its fist - crashing down upon where ",
-          "they once stood.")
-    print("“45 seconds”")    
-    print("As if it was some instinctive animal, it next charges at Will, although he would not succumb to fear and darts as fast as ,"
-          "he can. The wind howls as its fist tries to end him, barely avoiding Will as he sprints away. Clearly distressed by its ",
-          "failure it lets out a screech.")
-    print("“30 seconds”")
-    print("It seems to lose interest in Will momentarily, as the other candidate was banging on the metal door in which he had come from. ",
-          "Not a moment later, it brings its massive foot the size of a car crashing down upon him. You were the last one standing…")
+time.sleep(5)
 
-###########################################    
-if choice2 == "door2":
-    print("")
-    print("Door 2) Will began to sweat as he approached the void like room with an almost endless vertical drop. At",
-    "the bottom of the pit was a vast pool of molten iron which dimly illuminated the room.",
-    "Across the center was one singular path Will would have to cross to exit the room.",
-    "Without taking a breath Will wobbled over the rope worried he would fall. He reached a larger platform",
-    "for and for a brief moment and re-collected his thoughts. The next obstacle was a beam elevated above",
-    "with handles to climb the gap with great effort Will had traversed across to the next obstacle, a large",
-    "sheet metal wall with small gaps to fit fingers and feet in order to climb.",
-    "Will could see scratches and indents in the metal which looked to be caused by a type of climbing tool. In",
-    "his brief rest to evaluate his situation, a spinning red light activated as well as a metal piston that began",
-    "to slowly slide the wall behind him forwards. Now under the time constraint of the wall Will began to",
-    "panic. Will decides too...")
-##############################################
-# door 3 ( your part )
-if choice2 == "door3":
-    print("Will entered and was immediately punctured by a dart firing directly into his back, and he immediately felt a great burning pain as ",
-          "though his skin was on fire. In front, an extremely deep pool was made apparent to him, the bottom appearing so far away he could barely ",
-          "make out that there was a metal box at the bottom of the pool..")
-    print("")
-    print("“FOR THIS TASK YOU MUST SWIM TO THE BOTTOM, AND UNLOCK THE SAFE, THE SAFE IS LOCKED A SET OF LOGIC GATES IN WHICH MUST BE ALIGNED CORRECTLY ",
-          "TO UNLOCK THE ANTIDOTE TO THE DART YOU WERE SHOT WITH.”")
-    print("")
-    print("He looks down into the water barely lit up with equally spaced bulbs lining the side of the pool.")
-    print("Without hesitation, he dives right into the pool and swims as fast as he physically could downwards. The pressure became greater and greater ",
-          "and Will found it more and more of a struggle until finally he reached the bottom. He fought to remain at the bottom and looked at the logic ",
-          "gate on the side of the metal box. It had 2 inputs (A and B) which could be set to 0 or 1, A was followed by a NOT gate and then both A and B ",
-          "met at an AND gate. Will started to feel his chest tighten as he fought to hold his breath. Will fumbles and quickly begins setting the inputs.")
+# updated choices (choice2)
+
+check2 = False
+
+while check2 == False:
+    choice2 = input("(Go to door1 door2 or door3)")
+    
+    if choice2 == "door1":
+        output2 = choiceFunc(2)
+        check2 = True
+        
+    elif choice2 == "door2":
+        output2 = choiceFunc(3)
+        check2 = True
+        
+    elif choice2 == "door3":
+        output2 = choiceFunc(4)
+        check2 = True
+        
+    else:
+        print("Invalid option")
+
+print(output2)
 
 
